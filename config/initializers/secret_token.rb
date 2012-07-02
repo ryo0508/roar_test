51276b326ceda9605e393c5b9d7c2fa56488ee59
Ryo Sakikawa <ryo.sakikawa@gmail.com>
2012-07-02 21:02:54 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roar::Application.config.secret_token = '311292cc0ab59c39ae59eaeab999e25bd252f8f217de82fc32a80c9d4622c4ed94c3a5b3f535dc2c720a6bed4d4a092e89e61ea73ab06da0c15b6aa888142c6a'
